abb32c93e318f7306497a45bc9f0bb031734ac56
nera <neragr@gmail.com>
2023-03-07 22:51:54 +0100

    first commit

---
+++ gpt_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+import openai
+
+# Create your views here.
+openai.api_key="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"
+
+@csrf_exempt
+def main (request):
+    result=openai.Completion.create(
+        prompt=request.POST.get('prompt'),
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        model="text-davinci-003",
+    )
+    return JsonResponse(result)
